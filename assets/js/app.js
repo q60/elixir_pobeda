ce7fdfa2519699be71f53239891baeb1d2cad839
kira malinova <llathasa@outlook.com>
2025-03-02 13:59:28 +0300

    use ws instead of longpoll

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,7 @@ import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
-  longPollFallbackMs: 2500,
+  transports: ["websocket", "longpoll"],
   params: {_csrf_token: csrfToken}
 })
 
