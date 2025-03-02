1a51fe1e1ddce7c80ca11941aca78f5bacb7def6
kira malinova <llathasa@outlook.com>
2025-03-02 13:59:56 +0300

    mix format

--- test/elixir_pobeda_web/controllers/error_html_test.exs
+++ test/elixir_pobeda_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule ElixirPobedaWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(ElixirPobedaWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(ElixirPobedaWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
